785406fd1d0eb45750ff0da933b42000fae6c5ca
vl0ft <vyacheslav.lednev@gmail.com>
2019-06-24 07:17:27 +0300

    Add comments

--- src/main/java/org/yggdrasil/graph/Graph.java
+++ src/main/java/org/yggdrasil/graph/Graph.java
@@ -13,10 +13,29 @@ import java.util.List;
  */
 public interface Graph<R extends Edge<T>, T> {
 
-
+    /**
+     * Add Vertex to graph
+     *
+     * @param vertex
+     * @return return true if graph have vertex
+     */
     boolean addVertex(Vertex<T> vertex);
 
+    /**
+     * Add Edge to graph
+     *
+     * @param edge
+     * @return return true if graph have vertex from edge
+     */
+
     boolean addEdge(R edge);
+
+    /**
+     * Get path between two of vertexies
+     * @param v1
+     * @param v2
+     * @return return list of edge if path exists or null
+     */
 
     List<R> getPath(Vertex<T> v1, Vertex<T> v2);
 
